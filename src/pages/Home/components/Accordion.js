d94d48b0cdaf675837b49f5cfb2ba2ecb728a671
Dey11subrata <deysubrata2311@gmail.com>
2023-12-10 19:58:57 +0530

    answer dropdown updated

--- src/pages/Home/components/Accordion.js
+++ src/pages/Home/components/Accordion.js
@@ -14,10 +14,17 @@ export const Accordion = ({ faq }) => {
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-caret-up"
+              class="bi bi-chevron-double-up"
               viewBox="0 0 16 16"
             >
-              <path d="M3.204 11h9.592L8 5.519 3.204 11zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z" />
+              <path
+                fill-rule="evenodd"
+                d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+              />
             </svg>
           ) : (
             <svg
@@ -25,10 +32,17 @@ export const Accordion = ({ faq }) => {
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-caret-down"
+              class="bi bi-chevron-double-down"
               viewBox="0 0 16 16"
             >
-              <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" />
+              <path
+                fill-rule="evenodd"
+                d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+              />
             </svg>
           )}
         </button>
