6dba17fe2cd2fc6e4d2697dab4800f7bcf106a5b
Dey11subrata <deysubrata2311@gmail.com>
2023-12-13 22:57:04 +0530

    Products page layout

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages";
+import { Products } from "../pages/Products/Products";
 
 export const AllRoutes = () => {
   return (
     <div className="dark:bg-slate-800">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </div>
   );
